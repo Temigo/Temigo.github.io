e9a5486bd9d25a2cf5e66f81257d7eda7471306b
Temigo <temigo@gmx.com>
2018-07-09 01:22:36 -0700

    Updates

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-8910af3c5836f28805d1.js","e796dd0a823a89a6e97d476be345d469"],["/app-ae1a3a30977da027df3b.js","697421052d2fda172e7e6c76f205bbbe"],["/app-e4fd625daf9c646c70ae.js","16bf8522f7f6e5a9228cfe1f8d3c93a8"],["/app-f29454ecd2944489e649.js","3beb63454d1f13ea160a5e814cc96a88"],["/app-fcd118a5605970ba93fb.js","c700f37bf02e2e8ba1a1a438052d1c6d"],["/commons-134243b53d0960d5c84e.js","b70af5c3584e8feec39d9449d5fa111f"],["/commons-5080809176300c9d0153.js","ed901d31aadcb556b1f073d941be77f8"],["/index.html","675bf8c8c25c1033462980ac97fb2ebc"],["/offline-plugin-app-shell-fallback/index.html","06c41b682e064d9f6f9d8b2d1701c12e"]];
+var precacheConfig = [["/app-2061bdba9cc5d660c85d.js","535d689e8dc8c429a4f80ebd4e097f7c"],["/app-8910af3c5836f28805d1.js","e796dd0a823a89a6e97d476be345d469"],["/app-ae1a3a30977da027df3b.js","697421052d2fda172e7e6c76f205bbbe"],["/app-e4fd625daf9c646c70ae.js","16bf8522f7f6e5a9228cfe1f8d3c93a8"],["/app-f29454ecd2944489e649.js","3beb63454d1f13ea160a5e814cc96a88"],["/app-fcd118a5605970ba93fb.js","c700f37bf02e2e8ba1a1a438052d1c6d"],["/commons-134243b53d0960d5c84e.js","caea686123fb29f9efabe59da1071b49"],["/commons-5080809176300c9d0153.js","ed901d31aadcb556b1f073d941be77f8"],["/index.html","0e3e5c4c4644116e5451e98cb426c2a9"],["/offline-plugin-app-shell-fallback/index.html","8697d59bfaf53adec5d36d3aa9c5afbd"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
